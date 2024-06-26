984e1cffe9224b80f9b58f469ac64f9303cbab10
mytvDor <159150094+mytvDor@users.noreply.github.com>
2024-06-26 22:42:50 +0530

    Update AboutUs.jsx

--- src/components/about_us/AboutUs.jsx
+++ src/components/about_us/AboutUs.jsx
@@ -380,8 +380,11 @@ function AboutUs() {
 
             <div
               className="image"
-              style={{ minWidth: "350px", maxWidth: "40", overflow: "hidden" }}
-            >
+ style={{
+                maxWidth: "370px",
+                // height: "370px",
+                overflow: "hidden",
+              }}            >
               <img
                 src="https://static.wixstatic.com/media/c837a6_73b0c47b3d1549e4b96178367ef6c597~mv2.jpg/v1/fill/w_912,h_1510,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_73b0c47b3d1549e4b96178367ef6c597~mv2.jpg"
                 alt="imgageLeft"
